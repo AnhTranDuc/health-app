ca8ca5105b63b0971b541a7f2f743da8f3646998
AnhTranDuc <82199706+AnhTranDuc@users.noreply.github.com>
2023-03-16 11:14:04 +0700

    Columnpage (#2)
    * scrollToTop
    
    * scroolToTop
    
    * scroolToTop
    
    * responsive-columnpage

--- src/pages/column/Recommended.tsx
+++ src/pages/column/Recommended.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 export default function Recommended() {
   return (
-    <div className='container mt-[120px] flex gap-4'>
+    <div className='container px-4 mt-[120px] grid grid-cols-2 md:flex gap-4'>
         {listRecommended.map((item, i) => (
-        <div className='bg-[#2E2E2E] w-[25%] flex flex-col items-center box-border' key={i}>
-            <div className='w-[200px] text-center mt-[24px]'>
-                <p className='text-[#FFCC21] text-[22px] font-normal font-inter'>{item.label}</p>
+        <div className='bg-[#2E2E2E] flex flex-col items-center box-border w-full md:w-[25%] xl:px-8' key={i}>
+            <div className='md:w-full mt-[24px] xs:w-auto box-border sm:w-[50%] xs:[]'>
+                <p className='text-[#FFCC21] font-normal font-inter text-center text-[15px] lg:text-[22px] md:text-[18px] xs:text-[17px] sm:text-[20px] '>RECOMMENDED<br/>{item.label}</p>
             </div>
             <div className='w-[56px] border border-solid mt-[10px]'></div>
             <div className='mt-[8px] mb-[12px]'>
@@ -20,19 +20,19 @@ export default function Recommended() {
 
 const listRecommended = [
     {
-        label: 'RECOMMENDED COLUMN',
+        label: 'COLUMN',
         summary: 'オススメ'
     },
     {
-        label: 'RECOMMENDED DIET',
+        label: 'DIET',
         summary: 'ダイエット'
     },
     {
-        label: 'RECOMMENDED BEAUTY',
+        label: 'BEAUTY',
         summary: '美容'
     },
     {
-        label: 'RECOMMENDED HEALTH',
+        label: 'HEALTH',
         summary: '健康'
     },
 ]
